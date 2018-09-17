f21ce475544fad7dd3eac6c8a9fe236206b1ae58
Jens Ivar Jørdre <jijordre@gmail.com>
2018-09-17 16:42:08 +0200

    Add initial unit tests to be called from CI (#3)
    * Add initial test script and dependencies
    
    * Add npm script for the output of compiler version

--- contracts/test/TestMetacoin.sol
+++ contracts/test/TestMetacoin.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.2;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/MetaCoin.sol";
+import "../MetaCoin.sol";
 
 contract TestMetacoin {
 
