4281624f63e49c7e459d551d1496de1e0c75f13e
Jens Ivar Jordre <jensivar@hubii.com>
2018-09-21 11:25:01 +0200

    Remove MetaCoin stuff

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,7 @@
-const ConvertLib = artifacts.require('ConvertLib');
-const MetaCoin = artifacts.require('MetaCoin');
 const Oracle = artifacts.require('Oracle');
 const ResolutionEngine = artifacts.require('ResolutionEngine');
 
-module.exports = function (deployer) {
-    deployer.deploy(ConvertLib);
-    deployer.link(ConvertLib, MetaCoin);
-    deployer.deploy(MetaCoin);
-
+module.exports = (deployer) => {
     deployer.deploy(Oracle);
     deployer.deploy(ResolutionEngine);
 };
