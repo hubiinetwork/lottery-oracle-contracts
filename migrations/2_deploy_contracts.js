e7de7a974267ee49ce06e8b724a2c5e159c5ed72
Jens Ivar Jordre <jensivar@hubii.com>
2018-09-23 02:15:27 +0200

    Add stake tokens function
    In resolution engine add token property and setter, stakeTokens() and
    remove isSetOracle().
    
    Add test token.
    
    Async migration scripts.
    
    Add file header where missing.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,13 @@
+/*
+ * Lottery oracle
+ *
+ * Copyright (C) 2017-2018 Hubii AS
+ */
+
 const Oracle = artifacts.require('Oracle');
 const ResolutionEngine = artifacts.require('ResolutionEngine');
 
-module.exports = (deployer) => {
-    deployer.deploy(Oracle);
-    deployer.deploy(ResolutionEngine);
+module.exports = async (deployer) => {
+    await deployer.deploy(Oracle);
+    await deployer.deploy(ResolutionEngine);
 };
