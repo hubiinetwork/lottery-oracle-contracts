084aa8970f0dfc5f3008467443f37a06b1bb43c4
Jens Ivar Jordre <jensivar@hubii.com>
2019-09-20 18:59:19 +0200

    Undo use of factories

---
+++ migrations/6_naive-total-resolution-engine.js
@@ -0,0 +1,47 @@
+/*
+ * Lottery oracle
+ *
+ * Copyright (C) 2017-2019 Hubii AS
+ */
+
+'use strict';
+const utils = require('../script/common/utils.js');
+
+// Using './Contract.sol' rather than 'Contract' because of https://github.com/trufflesuite/truffle/issues/611
+const BountyFund = artifacts.require('./BountyFund.sol');
+const ConstantsLib = artifacts.require('./ConstantsLib.sol');
+const FractionalBalanceAllocator = artifacts.require('./FractionalBalanceAllocator.sol');
+const NaiveTotalResolutionEngine = artifacts.require('./NaiveTotalResolutionEngine.sol');
+const Oracle = artifacts.require('./Oracle.sol');
+const StakeToken = artifacts.require('./StakeToken.sol');
+const ResolutionEngineOperator = artifacts.require('./ResolutionEngineOperator.sol');
+const VerificationPhaseLib = artifacts.require('./VerificationPhaseLib.sol');
+
+module.exports = async (deployer, network, accounts) => {
+  const ownerAccount = await utils.initializeOwnerAccount(web3, network, accounts);
+
+  const stakeToken = await StakeToken.deployed();
+  const bountyFund = await deployer.deploy(BountyFund, stakeToken.address, {from: ownerAccount});
+
+  const bountyFraction = web3.utils.toBN(utils.getNaiveTotalBountyFraction());
+  await FractionalBalanceAllocator.link({
+    ConstantsLib: (await ConstantsLib.deployed()).address
+  });
+  const bountyAllocator = await deployer.deploy(FractionalBalanceAllocator, bountyFraction, {from: ownerAccount});
+
+  const oracle = await Oracle.deployed();
+  const operator = await ResolutionEngineOperator.deployed();
+  const amount = web3.utils.toBN(utils.getNaiveTotalCriterionAmount());
+  await NaiveTotalResolutionEngine.link({
+    VerificationPhaseLib: (await VerificationPhaseLib.deployed()).address
+  });
+  const resolutionEngine = await deployer.deploy(
+    NaiveTotalResolutionEngine, oracle.address, operator.address, bountyFund.address,
+    amount, {from: ownerAccount}
+  );
+  await resolutionEngine.setBountyAllocator(bountyAllocator.address);
+
+  // NOTE Remember to initialize resolution engine after deployment
+
+  await oracle.addResolutionEngine(resolutionEngine.address, {from: ownerAccount});
+};
--- migrations/7_alpha-beta-gamma-resolution-engine.js
+++ migrations/7_alpha-beta-gamma-resolution-engine.js
@@ -6,15 +6,12 @@
 
 'use strict';
 const utils = require('../script/common/utils.js');
-const {providers: {Web3Provider}} = require('ethers');
-const provider = new Web3Provider(web3.currentProvider);
-const confirm = require('../script/common/confirm')(provider);
 
 // Using './Contract.sol' rather than 'Contract' because of https://github.com/trufflesuite/truffle/issues/611
 const AlphaBetaGammaResolutionEngine = artifacts.require('./AlphaBetaGammaResolutionEngine.sol');
-const BountyFundFactory = artifacts.require('./BountyFundFactory.sol');
+const BountyFund = artifacts.require('./BountyFund.sol');
 const ConstantsLib = artifacts.require('./ConstantsLib.sol');
-const FractionalBalanceAllocatorFactory = artifacts.require('./FractionalBalanceAllocatorFactory.sol');
+const FractionalBalanceAllocator = artifacts.require('./FractionalBalanceAllocator.sol');
 const Oracle = artifacts.require('./Oracle.sol');
 const ResolutionEngineOperator = artifacts.require('./ResolutionEngineOperator.sol');
 const StakeToken = artifacts.require('./StakeToken.sol');
@@ -23,39 +20,32 @@ const VerificationPhaseLib = artifacts.require('./VerificationPhaseLib.sol');
 module.exports = async (deployer, network, accounts) => {
   const ownerAccount = await utils.initializeOwnerAccount(web3, network, accounts);
 
-  const bountyFundFactory = await BountyFundFactory.deployed();
-  const bountyAllocatorFactory = await FractionalBalanceAllocatorFactory.deployed();
-
   const stakeToken = await StakeToken.deployed();
-
-  await confirm((await bountyFundFactory.create(stakeToken.address)).tx);
-  const bountyFundsCount = await bountyFundFactory.instancesCount();
-  const bountyFund = await bountyFundFactory.instances(bountyFundsCount.subn(1));
+  const bountyFund = await deployer.deploy(BountyFund, stakeToken.address, {from: ownerAccount});
 
   const bountyFraction = web3.utils.toBN(utils.getAlphaBetaGammaBountyFraction());
-  await confirm((await bountyAllocatorFactory.create(bountyFraction)).tx);
-  const bountyAllocatorsCount = await bountyAllocatorFactory.instancesCount();
-  const bountyAllocator = await bountyAllocatorFactory.instances(bountyAllocatorsCount.subn(1));
+  await FractionalBalanceAllocator.link({
+    ConstantsLib: (await ConstantsLib.deployed()).address
+  });
+  const bountyAllocator = await deployer.deploy(FractionalBalanceAllocator, bountyFraction, {from: ownerAccount});
 
+  const oracle = await Oracle.deployed();
+  const operator = await ResolutionEngineOperator.deployed();
+  const alpha = web3.utils.toBN(utils.getAlphaBetaGammaCriterionAlpha());
+  const beta = web3.utils.toBN(utils.getAlphaBetaGammaCriterionBeta());
+  const gamma = web3.utils.toBN(utils.getAlphaBetaGammaCriterionGamma());
   await AlphaBetaGammaResolutionEngine.link({
     ConstantsLib: (await ConstantsLib.deployed()).address,
     VerificationPhaseLib: (await VerificationPhaseLib.deployed()).address
   });
-
-  const oracle = await Oracle.deployed();
-  const operator = await ResolutionEngineOperator.deployed();
-
-  const alpha = web3.utils.toBN(utils.getAlphaBetaGammaCriterionAlpha());
-  const beta = web3.utils.toBN(utils.getAlphaBetaGammaCriterionBeta());
-  const gamma = web3.utils.toBN(utils.getAlphaBetaGammaCriterionGamma());
   const resolutionEngine = await deployer.deploy(
-    AlphaBetaGammaResolutionEngine, oracle.address, operator.address, bountyFund,
+    AlphaBetaGammaResolutionEngine, oracle.address, operator.address, bountyFund.address,
     alpha, beta, gamma,
     {from: ownerAccount}
   );
-  await resolutionEngine.setBountyAllocator(bountyAllocator);
+  await resolutionEngine.setBountyAllocator(bountyAllocator.address);
 
-  // TODO Enable initialization after deployment
+  // NOTE Remember to initialize resolution engine after deployment
 
   await oracle.addResolutionEngine(resolutionEngine.address, {from: ownerAccount});
 };
